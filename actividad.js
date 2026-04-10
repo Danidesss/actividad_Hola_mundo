df448b655838ae7191b1444f86ffeb10a47c402e
Daniel Ledesma <161984128+Danidesss@users.noreply.github.com>
2026-04-09 21:55:58 -0400

    Pequeño contador que quise hacer por mi cuenta
    Hice un contador de 10 a 0 y que hiciera algo al llegar a 0

--- actividad.js
+++ actividad.js
@@ -10,3 +10,23 @@ console.log("Hola mundo del punto 4")
 
 //5
 document.getElementById("contenedorxd").innerHTML="hola del contenedor div porque tambien necesito identificarlos";
+
+
+
+let segundos = 10;
+
+
+const elementoContador = document.getElementById('contador');
+
+
+setInterval(function() {
+   if (segundos > 0 ){
+    segundos--; 
+    elementoContador.innerText = segundos;
+    console.log("Tiempo: " + segundos);
+   }
+   else{
+    document.getElementById("fincontadorxd").innerText="Feliz año nuevoooo";
+
+   }
+}, 1000);
