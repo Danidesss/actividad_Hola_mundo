0f7e4722ab5763bbdf7fab430402ca79e52b984c
Daniel Ledesma <161984128+Danidesss@users.noreply.github.com>
2026-04-09 21:41:13 -0400

    Actividad desarrollada
    Actividad de integracion a Javascript, realizando asignaciones de texto a contenedores con id en html

---
+++ actividad.js
@@ -0,0 +1,12 @@
+//1
+console.log("El script se cargó correctamente");
+alert("Hola Mundoxddd");
+
+//2
+document.getElementById("mensaje").innerText="Hola mundo con innertext porque necesito identificarlos"
+
+//4
+console.log("Hola mundo del punto 4")
+
+//5
+document.getElementById("contenedorxd").innerHTML="hola del contenedor div porque tambien necesito identificarlos";
